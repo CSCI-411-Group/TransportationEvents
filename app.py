4b0320ed2e4f12a6d068c6b32dd9a4379be23e7c
khaled shaheen <kshaheen2003@gmail.com>
2023-11-03 20:31:58 -0500

    5 b and c done

--- app.py
+++ app.py
@@ -22,27 +22,44 @@ def create():
 @app.route('/search', methods=['GET'])
 def search():
     person_id = request.args.get('personId')
-    
-    # Connect to your postgres DB
-    conn = psycopg2.connect(**db_config)
+    event_link_id = request.args.get('linkId')
+    link_id_table = request.args.get('linkIdLinkTable')
+    try:
+        # Connect to your postgres DB
+        conn = psycopg2.connect(**db_config)
+        # Open a cursor to perform database operations
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-    # Open a cursor to perform database operations
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        results_list = []
 
-    # Execute a query
-    cur.execute("SELECT * FROM Events WHERE Person = %s ORDER BY Time ASC", (person_id,))
+        if person_id:
+            cur.execute("SELECT * FROM Events WHERE Person = %s ORDER BY Time ASC", (person_id,))
+            results = cur.fetchall()
+            results_list.extend([dict(event) for event in results])
 
-    # Retrieve query results
-    events = cur.fetchall()
+        if event_link_id:
+            cur.execute("SELECT * FROM Events WHERE Link = %s ORDER BY Time ASC", (event_link_id,))
+            results = cur.fetchall()
+            results_list.extend([dict(event) for event in results])
 
-    # Close communication with the database
-    cur.close()
-    conn.close()
+        if link_id_table:
+            cur.execute("SELECT * FROM Links WHERE LinkID = %s", (link_id_table,))
+            results = cur.fetchall()
+            results_list.extend([dict(link) for link in results])
 
-    # Convert the events to a list of dicts to jsonify
-    events_list = [dict(event) for event in events]
-    
-    return jsonify(events_list)
+        if not results_list:
+            return jsonify({'error': 'Invalid or missing search parameters'}), 400
+
+    except Exception as e:
+        return jsonify({'error': 'Database operation failed', 'details': str(e)}), 500
+    finally:
+        # Close communication with the database if open
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
+
+    return jsonify(results_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,30 +1,53 @@
 $(document).ready(function() {
     var searchForm = $('#search-form');
+    
+    $('input[name="searchType"]').change(function() {
+        // Enable the appropriate input based on the selected search type
+        var searchType = $(this).val();
+        if(searchType === 'personId') {
+            $('#personId').prop('disabled', false);
+            $('#linkId').prop('disabled', true);
+            $('#linkIdLinkTable').prop('disabled', true);
+
+        } else if (searchType === 'linkId') {
+            $('#personId').prop('disabled', true);
+            $('#linkId').prop('disabled', false);
+            $('#linkIdLinkTable').prop('disabled', true);
+        }
+        else{
+            $('#personId').prop('disabled', true);
+            $('#linkId').prop('disabled', true);
+            $('#linkIdLinkTable').prop('disabled', false);
+        }
+    });
+
     searchForm.on('submit', function(e) {
         e.preventDefault();
-        var personId = $('#personId').val();
-        searchEvents(personId);
+        var searchType = $('input[name="searchType"]:checked').val();
+        var searchId = $('#' + searchType).val();
+        searchEvents(searchType, searchId);
     });
 });
 
-function searchEvents(personId) {
-    fetch('/search?personId=' + encodeURIComponent(personId))
+function searchEvents(searchType, searchId) {
+    var queryParam = searchType + '=' + encodeURIComponent(searchId);
+    fetch('/search?' + queryParam)
         .then(response => response.json())
         .then(events => {
-            // Construct the table if not already present
+               // Construct the table if not already present
             if ($.fn.DataTable.isDataTable('#eventsTable')) {
                 $('#eventsTable').DataTable().destroy();
             }
             $('#events').empty();
-
-            let tableHtml = `
-                <table id="eventsTable" class="display">
+            var tableHtml;
+            if(searchType == 'personId' || searchType == 'linkId'){
+                tableHtml = `
+                <table id="eventsTable" class="display centered-table">
                     <thead>
                         <tr>
                             <th>Event ID</th>
                             <th>Type</th>
                             <th>Time</th>
-                            <th>Link</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -33,21 +56,56 @@ function searchEvents(personId) {
                                 <td>${event.eventid}</td>
                                 <td>${event.type}</td>
                                 <td>${event.time}</td>
-                                <td>${event.link}</td>
                             </tr>
                         `).join('')}
                     </tbody>
                 </table>
             `;
+            }             
+            else{
+                tableHtml = `
+                <table id="eventsTable" class="display centered-table">
+                    <thead>
+                        <tr>
+                            <th>FreeSpeed</th>
+                            <th>Capacity</th>
+                            <th>Mode</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        ${events.map(event => `
+                            <tr>
+                                <td>${event.freespeed}</td>
+                                <td>${event.capacity}</td>
+                                <td>${event.mode}</td>
+                            </tr>
+                        `).join('')}
+                    </tbody>
+                </table>
+            `;
+            }
 
             $('#events').html(tableHtml);
             // Initialize DataTables
             $('#eventsTable').DataTable({
-                "order": [[2, "asc"]] // Assuming the third column is 'Time' and we want to sort by it
+                // DataTables initialization options
+                destroy: true, // This option allows re-initialization on the same table
+                autoWidth: false // This can help with rendering issues in some cases
+                
             });
+          
         })
         .catch(error => {
             console.error('Error:', error);
             $('#events').html('Error fetching events.');
         });
 }
+
+
+
+
+
+
+
+
+
